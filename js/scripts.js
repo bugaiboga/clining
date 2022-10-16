e8e4ef82618a0158cd4bcffced241da02b49a5ec
bugai bogdan <bugaiboga@gmail.com>
2022-10-16 07:20:01 +0300

    completed sliders

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(".work-slider").slick({
+  prevArrow:
+    '<button type="button" class="slick-prev slick-btn b-0 pa"><img src="./images/vectors/arrow-left.svg" alt="arrow"></button>',
+  nextArrow:
+    '<button type="button" class="slick-next slick-btn b-0 pa"><img src="./images/vectors/arrow-right.svg" alt="arrow"></button>',
+});
+$(".about-us-slider").slick({
+  prevArrow:
+    '<button type="button" class="slick-prev slick-btn b-0 pa"><img src="./images/vectors/arrow-left.svg" alt="arrow"></button>',
+  nextArrow:
+    '<button type="button" class="slick-next slick-btn b-0 pa"><img src="./images/vectors/arrow-right.svg" alt="arrow"></button>',
+});
+$(".team-slider").slick({
+  arrows: false,
+  dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+});
+$(".reviews-slider").slick({
+  dots: true,
+  prevArrow:
+    '<button type="button" class="slick-prev slick-btn b-0 pa"><img src="./images/vectors/arrow-left.svg" alt="arrow"></button>',
+  nextArrow:
+    '<button type="button" class="slick-next slick-btn b-0 pa"><img src="./images/vectors/arrow-right.svg" alt="arrow"></button>',
+});
