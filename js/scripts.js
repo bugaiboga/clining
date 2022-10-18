56d8f27c394aad626c3d5708065b2fc2dad3b43f
bugai bogdan <bugaiboga@gmail.com>
2022-10-18 09:01:02 +0300

    completed media-queries

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,25 @@ $(".team-slider").slick({
   dots: true,
   slidesToShow: 4,
   slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+  ],
 });
 $(".reviews-slider").slick({
   dots: true,
@@ -22,4 +41,31 @@ $(".reviews-slider").slick({
     '<button type="button" class="slick-prev slick-btn b-0 pa"><img src="./images/vectors/arrow-left.svg" alt="arrow"></button>',
   nextArrow:
     '<button type="button" class="slick-next slick-btn b-0 pa"><img src="./images/vectors/arrow-right.svg" alt="arrow"></button>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: false,
+      },
+    },
+  ],
 });
+
+let navMenu = document.querySelector("#nav-header");
+let btnMenu = document.querySelector("#menu-burger");
+
+btnMenu.onclick = function () {
+  navMenu.classList.toggle("d-opasity");
+};
